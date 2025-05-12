a7fba43538f6ee427a2df6eed414e3598c655a6e
presentheo.newstapa <presentheo@newstapa.org>
2025-05-12 15:42:46 +0900

    불필요한 파일 삭제, 빌드용 옵션으로 변경

--- fc2025/src/components/SafeImage.tsx
+++ fc2025/src/components/SafeImage.tsx
@@ -7,7 +7,7 @@ type SafeImageProps = ImageProps & {
   fallbackSrc: string;
 }
 
-export default function SafeImage({ src, alt, fallbackSrc='/images/candidate/def.jpg', ...props }: SafeImageProps) {
+export default function SafeImage({ src, alt, fallbackSrc='/2025/factcheck2025/images/candidate/def.jpg', ...props }: SafeImageProps) {
   const [imgSrc, setImgSrc] = useState(src);
 
   return (
